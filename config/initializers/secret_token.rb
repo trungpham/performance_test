32c25e38f32c3f695496fc9ffd2e73dce62eeb45
Chad Johnson <chad.d.johnson@gmail.com>
2013-11-01 17:05:25 -0500

    Created a new Rails project with various dependencies added (gems and JavaScript libraries).

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PerformanceTest::Application.config.secret_key_base = 'ca9e89a0c0f5f925eb190a105a34cad0bc96e256727a37fe2c9b63af5f3061455273950dadde9606c7adbcc2b6c1f00f5cfcacb99617b68a1e8c65c514dc14a3'
